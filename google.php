d8a1073671340954ffcd80654a58e259248a71a9
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-03-29 04:12:05 +0100

    docupd

--- google.php
+++ google.php
@@ -4,16 +4,19 @@
 
 require './vendor/autoload.php';
 
-// **********************************************************CLIENT ID
+// ********************************************************** CLIENT ID
 // ****************************************** CLIENT SECRET
 
-//scopes needed for this client query -|> Google Service Search Console 
-//scope https://www.googleapis.com/auth/webmasters.readonly
-//scope https://www.googleapis.com/auth/webmasters
+// Note: Dont forget to upload SECRET CLIENT ID JSON into the folder of this project like the example shown. 
 
-session_start(); // session opening
+// Scopes needed for this client query -> Google Service Search Console 
 
-//client google
+// scope https://www.googleapis.com/auth/webmasters.readonly
+// scope https://www.googleapis.com/auth/webmasters
+
+session_start(); // Session Opening (essential for tokem verification)
+
+//Googgle Client
 $client = new Google_Client();
 $client->setApplicationName('Google Search Console API PHP');
 $client->setScopes([
@@ -21,7 +24,7 @@ $client->setScopes([
     'https://www.googleapis.com/auth/webmasters'
 ]);
 $client->setAccessType('offline');
-$client->setPrompt('consent'); // in the case of google constant demand for verification, just comment it
+$client->setPrompt('consent'); // In the case of google constant demand for verification, just comment this line. 
 $client->setAuthConfig('yourgoogleusersecretjson.com.json');
 $client->setRedirectUri('https://www.example.fr/path/to/your/folder/google.php');
 
@@ -42,7 +45,7 @@ if (!empty($_SESSION['access_token'])) {
     $client->setAccessToken($_SESSION['access_token']);
 
     if ($client->isAccessTokenExpired()) {
-        // it assures a token is already in place
+        // It assures a token is already in place
         if (!empty($client->getRefreshToken())) {
             $newAccessToken = $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
             $_SESSION['access_token'] = $client->getAccessToken();
@@ -52,6 +55,7 @@ if (!empty($_SESSION['access_token'])) {
     }
 }
 
+//If doule security measures are needed, use a slug with a security code and set the url a txt file inside the projects folder.  This is not being used in this client model. 
 
 if (isset($_GET['code'])) {
     $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
@@ -68,23 +72,23 @@ if (isset($_GET['code'])) {
     }
 }
 
-// access token = successful authentification
+// Access token present = successful authentification
 if ($client->getAccessToken()) {
-
 
     // echo "Gogggle Search Console Keyword Analysis";
 
-    //access gsc par client
-    $service = new Google_Service_SearchConsole($client); //this class might give an error, if php and composers versions dont match with your machine ones. 
-    //Ignore it in the case of SFTP connection like this one wich was made for a prestashop server. 
+    //Access Search Console by client API
+    $service = new Google_Service_SearchConsole($client); //This class might give an error, if php and composers versions dont match with your machine ones. 
+    
+    //If the machine has  older versions of php and composer then DON'T update composer in the project terminal. Always verify your server versions.  
 
 
-    $siteUrl = 'https://www.example.com/'; // the website we added to google clients credentials and wich we had granted permissino in oogle search console
+    $siteUrl = 'https://www.example.com/'; // The website we added to google clients credentials with permission ownership in our Google Search Console Account.
 
     // NEW SEARCH query
     $query = new Google_Service_SearchConsole_SearchAnalyticsQueryRequest(); //ignore php composer mismatches
 
-    $query->setStartDate('2024-01-01'); // start date
+    $query->setStartDate('2024-01-01'); // start date 
     $query->setEndDate(date("Y-m-d")); // end date
     $query->setDimensions(['page', 'query']); // dimensions query
     $query->setRowLimit(100); // number of queries
@@ -115,7 +119,9 @@ if ($client->getAccessToken()) {
         echo 'Caught exception: ',  $e->getMessage(), "\n";
     }
 } else {
+
     // Redirect to Google's OAuth 2.0 server to initiate the authentication and authorization process
+
     $authUrl = $client->createAuthUrl();
     header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
     exit();
